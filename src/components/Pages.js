b02b3c7704a68f522d6a435c1baed9bf20ef6901
ejjoj <szymaskibartosz@gmail.com>
2020-09-12 13:00:22 +0200

    HomePage.js desktop

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import RegisterPage from "../pages/RegisterPage";
 
 const Pages = () => {
   return (
     <Switch>
       <Route path='/' exact component={HomePage} />
+      <Route path='/about' component={AboutPage} />
+      <Route path='/register' component={RegisterPage} />
     </Switch>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,17 +7,32 @@ import '../styles/HomePage.scss';
 const HomePage = () => {
   return (
     <div className="row home">
-      <div className="col-md-9 home-lead">
-        <h1 className='home-title'>Twoja aplikacja do organizacji <em className='home-title__small'>swojego</em> świata!</h1>
-        <p className="home-text">
-          Cześć, dzięki że jesteś. Chciałem Ci powiedzieć po krótce co tutaj robię. Mianowicie ta aplikacja współpracuje z REST API, dzięki któremu zapisywanie swoich zadań na dłużej to nie problem. Jest to swojego rodzaju test moich umiejętności jako Fullstack Developera. Narzędzia, które zostały użyte: <strong>React.js</strong>, <strong>PHP</strong>, <strong>SCSS</strong>, <strong>Bootstrap</strong>.
-          Zapraszam do testowania. Po swojej przygodzie chciałbym, abyś podzielił się ze mną tym, co myślisz o całej aplikacji. Każda uwaga jest milewidziana. Miłego dnia =)
-        </p>
+      <div className="col-md-6 home-lead">
+        <h1 className='home-title'>To-Do-App, zorganizuj swój czas.</h1>
+        <p className="home-text"><span className="circle"></span>Cześć, dzięki że jesteś.</p>
+        <p className="home-text"><span className='circle'></span>Chciałem Ci powiedzieć po krótce co tutaj robię. Mianowicie ta aplikacja współpracuje z REST API, dzięki któremu zapisywanie swoich zadań na dłużej to nie problem. Jest to swojego rodzaju test moich umiejętności jako Fullstack Developera.</p>
+        <p className="home-text">Zapraszam do testowania. Po swojej przygodzie chciałbym, abyś podzielił się ze mną tym, co myślisz o całej aplikacji. Każda uwaga jest mile widziana.<span className='circle'></span></p>
+        <p className="home-text"><span className='circle'></span>Narzędzia, które zostały użyte: <strong>React.js</strong>, <strong>PHP</strong>, <strong>SCSS</strong>, <strong>Bootstrap</strong>.</p>
+        <p className="home-text">Miłego używania!</p>
       </div>
-      <div className="col-md-3 home-image">
+      <div className="col-md-6 home-image">
         <figure className='home-image__container'>
           <img className='home-image__picture' src={list} alt="lista"/>
         </figure>
+      </div>
+      <div className="col-md-12 home-technologies">
+        <div className="col-md-2 home-technology">
+          <i className="fab fa-react"></i>
+        </div>
+        <div className="col-md-2 home-technology">
+          <i className="fab fa-php"></i>
+        </div>
+        <div className="col-md-2 home-technology">
+          <i className="fab fa-sass"></i>
+        </div>
+        <div className="col-md-2 home-technology">
+          <i className="fab fa-bootstrap"></i>
+        </div>
       </div>
     </div>
   );
